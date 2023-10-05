ce5ddeea3dfd701b88778082ca3f1d02fd6b743b
Dima-Kaidash <17254dima@gmail.com>
2023-10-05 20:08:42 +0300

    first commit

---
+++ src/main/java/com/kaidash/datingapp/entity/Users.java
@@ -0,0 +1,40 @@
+package com.kaidash.datingapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Users users = (Users) o;
+        return id != null && Objects.equals(id, users.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
