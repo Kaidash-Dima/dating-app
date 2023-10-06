21616d6334a30bf086cf694bb72eb79f63c10e5f
Dima-Kaidash <17254dima@gmail.com>
2023-10-06 15:06:43 +0300

    add configurtion C0RS

--- src/main/java/com/kaidash/datingapp/DatingApp.java
+++ src/main/java/com/kaidash/datingapp/DatingApp.java
@@ -2,6 +2,9 @@ package com.kaidash.datingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DatingApp {
@@ -10,5 +13,20 @@ public class DatingApp {
 		SpringApplication.run(DatingApp.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Allow URL patterns. /** means all endpoints in the application.
+						.allowedOrigins("*") // Origins that are allowed to access.
+						.allowedMethods("GET", "POST", "PUT", "DELETE") // HTTP methods that are allowed. Add or remove as per your need.
+						.allowCredentials(true)
+						.allowedHeaders("*") // Allow all headers.
+						.exposedHeaders("header1", "header2", "header3")
+						.maxAge(3600); // Cache the CORS configuration for 1 hour.
+			}
+		};
+	}
 }
 
